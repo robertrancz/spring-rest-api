a6e0fcba3ba30b4fe650a5f3758e14d4dbd7f070
Robert Rancz <rancz.robi@gmail.com>
2021-04-12 22:53:56 +0300

    Add i18n

--- restful-web-services/src/main/java/com/rrobi/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/rrobi/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,10 +1,16 @@
 package com.rrobi.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	// @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 	@GetMapping(path = "/hello-world")
@@ -17,5 +23,11 @@ public class HelloWorldController {
 	public HelloWorldBean Hello() {
 		
 		return new HelloWorldBean("Hello World!");
+	}
+	
+	@GetMapping(path = "/good-morning-i18n")
+	public String GoodMorningInternationalized() {
+		
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
