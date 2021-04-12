109ba4aa582c5396aef42f0de61b186f08a0fe31
Robert Rancz <rancz.robi@gmail.com>
2021-04-12 16:16:07 +0300

    Add user validation. Handle validation errors.

--- restful-web-services/src/main/java/com/rrobi/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ restful-web-services/src/main/java/com/rrobi/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,10 @@ package com.rrobi.restfulwebservices.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +30,12 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		var exResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 		
 		return new ResponseEntity<Object>(exResponse, HttpStatus.NOT_FOUND);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		var exResponse = new ExceptionResponse(new Date(), "Validation failed", ex.getBindingResult().toString());
+		return new ResponseEntity<Object>(exResponse, HttpStatus.BAD_REQUEST);
 	}
 }
--- restful-web-services/src/main/java/com/rrobi/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/rrobi/restfulwebservices/user/User.java
@@ -2,10 +2,17 @@ package com.rrobi.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 	
 	private Integer id;
+	
+	@Size(min = 2, message = "User name should have minimum 2 characters.")
 	private String name;
+	
+	@Past(message = "Birth date must be in the past.")
 	private Date birthDate;	
 	
 	public User(Integer id, String name, Date birthDate) {
